0ba34afc15347d91a770333079287d0222ff96d8
MadsBechP <m.bechpetersen@gmail.com>
2025-09-22 13:56:58 +0200

    Minigame complete

--- Assets/Content/PolarBearMinigame/FishingHoleLogic.cs
+++ Assets/Content/PolarBearMinigame/FishingHoleLogic.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class FishingHoleLogic : MonoBehaviour
+public class FishingHoleLogic : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private GameObject Fish;
     [SerializeField] private GameObject Hole;
+
+    private bool active;
+    
     void Start()
     {
 
@@ -13,13 +17,24 @@ public class FishingHoleLogic : MonoBehaviour
     {
         
     }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (active)
+        {
+            Fish.SetActive(false);
+            PolarBearGameManager.Instance.SendMessage("IncreaseScore", SendMessageOptions.DontRequireReceiver);
+            PolarBearLogic.Instance.SendMessage("Eat", gameObject, SendMessageOptions.DontRequireReceiver);
+        }
+    }
 
     public void deactivateFish()
     {
+        active = false;
         Fish.SetActive(false);
     }
     public void activateFish()
     {
+        active = true;
         Fish.SetActive(true);
     }
 }
--- Assets/Content/PolarBearMinigame/PolarBearGameManager.cs
+++ Assets/Content/PolarBearMinigame/PolarBearGameManager.cs
@@ -1,36 +1,98 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class PolarBearGameManager : MonoBehaviour
+public class PolarBearGameManager : MonoBehaviour, IMinigame
 {
-    //[SerializeField] private GameObject fishingHoleList;
-    //private List<FishingHoleLogic> fishingHoles = new List<FishingHoleLogic>();
-    //private List<FishingHoleLogic> activatedfishingHoles = new List<FishingHoleLogic>();
-    //private float timer = 0;
+    public static PolarBearGameManager Instance { get; private set; }
 
+    private bool started = false;
+    [SerializeField] private GameObject fishingHoleList;
+    private List<GameObject> fishingHoles = new List<GameObject>();
+    private List<GameObject> activatedfishingHoles = new List<GameObject>();
+
+    [Header("Game Settings")]
+    private float timer = 0;
+    [SerializeField]private float timerLength = 1;
+    private int fishCurrent = 0;
+    [SerializeField]private int fishGoal = 20;
+
+    [Header("UI Settings")]
+    [SerializeField] UIDocument PolarBearUIObject;
+    private Label scoreLabel;
+
+    [Header("Save Settings")]
+    [SerializeField] private SaveSO saveSO;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     void Start()
     {
-        //foreach (Transform child in fishingHoleList.transform)
-        //{
-        //    fishingHoles.Add(child.gameObject.GetComponent<FishingHoleLogic>);
-        //}
+        var root = PolarBearUIObject.rootVisualElement;
+        scoreLabel = root.Q<Label>("Score");
+        scoreLabel.text = $"Score 0 / {fishGoal}";
+
+        foreach (Transform child in fishingHoleList.transform)
+        {
+            fishingHoles.Add(child.gameObject);
+        }
+        foreach (GameObject hole in fishingHoles)
+        {
+            hole.SendMessage("deactivateFish", SendMessageOptions.DontRequireReceiver);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //    timer += Time.deltaTime;
-        //    if (timer >= 1)
-        //    {
-        //        foreach (GameObject hole in activatedfishingHoles)
-        //        {
-        //            hole.deac
-        //        }
-        //        foreach(GameObject hole in fishingHoles)
-        //        {
-        //            Random.Range(0,2)
-        //        }
-        //    }
+        if (started)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timerLength)
+            {
+                foreach (GameObject hole in activatedfishingHoles)
+                {
+                    hole.SendMessage("deactivateFish", SendMessageOptions.DontRequireReceiver);
+                }
+                foreach (GameObject hole in fishingHoles)
+                {
+                    if (Random.Range(0, fishingHoles.Count - 1) == 0)
+                    {
+                        hole.SendMessage("activateFish", SendMessageOptions.DontRequireReceiver);
+                        activatedfishingHoles.Add(hole);
+                    }
+                }
+                timer = 0;
+            } 
+        }
 
+    }
+    void IncreaseScore()
+    {
+        fishCurrent++;
+
+        if (fishCurrent >= fishGoal)
+        {
+            fishCurrent = fishGoal;
+            started = false;
+            Minigame_UI_Script.Instance.ShowVictoryPopup();
+            if (saveSO.saveData.TryGetValue("PolarBear", out bool value))
+            {
+                saveSO.saveData["PolarBear"] = true;
+            }
+        }
+        scoreLabel.text = $"Score {fishCurrent} / {fishGoal}";
+    }
+
+    public void StartGame()
+    {
+        started = true;
+        fishCurrent = 0;
     }
 }
