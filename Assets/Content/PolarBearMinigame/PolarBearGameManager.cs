f4391dfd6adf5c42f3dcd0e637a8570aa4a3bc77
MadsBechP <m.bechpetersen@gmail.com>
2025-09-22 12:21:51 +0200

    ForMyHusband

--- Assets/Content/PolarBearMinigame/PolarBearGameManager.cs
+++ Assets/Content/PolarBearMinigame/PolarBearGameManager.cs
@@ -3,34 +3,34 @@ using UnityEngine;
 
 public class PolarBearGameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject fishingHoleList;
-    private List<FishingHoleLogic> fishingHoles = new List<FishingHoleLogic>();
-    private List<FishingHoleLogic> activatedfishingHoles = new List<FishingHoleLogic>();
-    private float timer = 0;
+    //[SerializeField] private GameObject fishingHoleList;
+    //private List<FishingHoleLogic> fishingHoles = new List<FishingHoleLogic>();
+    //private List<FishingHoleLogic> activatedfishingHoles = new List<FishingHoleLogic>();
+    //private float timer = 0;
 
     void Start()
     {
-        foreach (Transform child in fishingHoleList.transform)
-        {
-            fishingHoles.Add(child.gameObject.GetComponent<FishingHoleLogic>);
-        }
+        //foreach (Transform child in fishingHoleList.transform)
+        //{
+        //    fishingHoles.Add(child.gameObject.GetComponent<FishingHoleLogic>);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= 1)
-        {
-            foreach (GameObject hole in activatedfishingHoles)
-            {
-                hole.deac
-            }
-            foreach(GameObject hole in fishingHoles)
-            {
-                Random.Range(0,2)
-            }
-        }
+        //    timer += Time.deltaTime;
+        //    if (timer >= 1)
+        //    {
+        //        foreach (GameObject hole in activatedfishingHoles)
+        //        {
+        //            hole.deac
+        //        }
+        //        foreach(GameObject hole in fishingHoles)
+        //        {
+        //            Random.Range(0,2)
+        //        }
+        //    }
 
     }
 }
